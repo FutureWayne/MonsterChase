ac0f32248ca2ec297c52e8a4dcd0494dccaf13a0
Future Wayne <futurewayne.ludens@gmail.com>
2023-10-21 19:05:57 -0600

    update logic

--- MonsterChase/Engine/HeapManager.cpp
+++ MonsterChase/Engine/HeapManager.cpp
@@ -1,9 +1,28 @@
 #include "HeapManager.h"
 #include <Windows.h> // For HeapAlloc and VirtualAlloc
-#include <cassert>
+#include <cstdio>
+#include <tuple>
 
 namespace HeapManagerProxy
 {
+
+	HeapManager* CreateHeapManager(void* pHeapMemory, size_t heapSize, unsigned int numDescriptors)
+	{
+		assert(pHeapMemory != nullptr);
+		assert(heapSize > 0);
+		
+		HeapManager* pHeapManager = new HeapManager(pHeapMemory, heapSize, numDescriptors);
+
+		return pHeapManager;
+	}
+	
+	void Destroy(HeapManager* pHeapManager)
+	{
+		assert(pHeapManager != nullptr);
+		
+		delete pHeapManager;
+	}
+	
 	HeapManager::HeapManager(void* pHeapMemory, size_t heapSize, unsigned numDescriptors)
 	{
 		assert(pHeapMemory != nullptr);
@@ -17,63 +36,82 @@ namespace HeapManagerProxy
 
 		// Initialize the linked list of outstanding allocations (empty at the start)
 		pOutstandingAllocationList = nullptr;
+
+		this->theHeapSize = heapSize;
+
+		pEntireHeap = pFreeMemoryBlockList;
 	}
 
 	HeapManager::~HeapManager()
     {
-		// Cleanup logic here, like releasing allocated memory blocks and the heap itself
+		delete pEntireHeap;
 	}
-    
 
-	void* HeapManager::alloc(size_t size)
+
+	std::pair<MemoryBlock*, MemoryBlock*> HeapManager::findFreeBlock(size_t size)
 	{
-		assert(size > 0);
-
 		MemoryBlock* currentBlock = pFreeMemoryBlockList;
 		MemoryBlock* previousBlock = nullptr;
-
 		while (currentBlock)
 		{
 			if (currentBlock->BlockSize >= size)
 			{
-				// Found a suitable block
-
-				// Adjust the size of the current block
-				currentBlock->BlockSize -= size;
-
-				// Create a new block for the allocation
-				MemoryBlock* newBlock = nullptr;
-				newBlock->pBaseAddress = currentBlock->pBaseAddress;
-				newBlock->BlockSize = size;
-
-				// track allocation
-				newBlock->pNextBlock = pOutstandingAllocationList;
-				pOutstandingAllocationList = newBlock;
-
-				// Adjust the free memory block list
-				if (currentBlock->BlockSize == 0)
-				{
-					if (previousBlock)
-					{
-						previousBlock->pNextBlock = currentBlock->pNextBlock;
-					}
-					else
-					{
-						pFreeMemoryBlockList = currentBlock->pNextBlock;
-					}
-				}
-
-				return newBlock->pBaseAddress;
+				return {currentBlock, previousBlock};
 			}
-
 			previousBlock = currentBlock;
 			currentBlock = currentBlock->pNextBlock;
 		}
-
-		// No suitable block found
-		return nullptr;
+		return {nullptr, nullptr};
 	}
+	
+	void* HeapManager::alloc(size_t size)
+	{
+		assert(size > 0);
+		
+		auto [suitableBlock, previousBlock] = findFreeBlock(size);
 
+		// If no suitable block is found, attempt to defragment the heap
+		if (!suitableBlock)
+		{
+			Collect();
+			std::tie(suitableBlock, previousBlock) = findFreeBlock(size);
+		}
+
+		// If a suitable block is still not found after defragmentation, return nullptr
+		if (!suitableBlock)
+		{
+			return nullptr;
+		}
+		
+
+		MemoryBlock* newBlock = suitableBlock;
+		newBlock->pBaseAddress = suitableBlock->pBaseAddress;
+		newBlock->BlockSize = size;
+
+		// Adjust the suitableBlock to account for the allocated memory
+		suitableBlock->pBaseAddress = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(suitableBlock->pBaseAddress) + size);
+		suitableBlock->BlockSize -= size;
+		
+		// If the block's size is reduced to 0, remove it from the free list
+		if (suitableBlock->BlockSize == 0)
+		{
+			if (previousBlock)
+			{
+				previousBlock->pNextBlock = suitableBlock->pNextBlock;
+			}
+			else
+			{
+				pFreeMemoryBlockList = suitableBlock->pNextBlock;
+			}
+		}
+
+		// track allocation
+		newBlock->pNextBlock = pOutstandingAllocationList;
+		pOutstandingAllocationList = newBlock;
+
+		return newBlock->pBaseAddress;
+	}
+	
     bool HeapManager::free(void* ptr)
     {
         assert(ptr);
@@ -112,6 +150,101 @@ namespace HeapManagerProxy
         // Block not found in the outstanding allocation list
         return false;
     }
+
+	void HeapManager::ShowFreeBlocks() const
+	{
+		printf("Free Blocks:\n");
+		MemoryBlock* currentBlock = pFreeMemoryBlockList;
+		while (currentBlock)
+		{
+			printf("Base Address: %p, Size: %zu bytes\n", currentBlock->pBaseAddress, currentBlock->BlockSize);
+			currentBlock = currentBlock->pNextBlock;
+		}
+	}
+
+	void HeapManager::ShowOutstandingAllocations() const
+	{
+		printf("Outstanding Allocations:\n");
+		MemoryBlock* currentBlock = pOutstandingAllocationList;
+		while (currentBlock)
+		{
+			printf("Base Address: %p, Size: %zu bytes\n", currentBlock->pBaseAddress, currentBlock->BlockSize);
+			currentBlock = currentBlock->pNextBlock;
+		}
+	}
+
+	size_t HeapManager::GetLargestFreeBlock() const
+	{
+		size_t largestSize = 0;
+		MemoryBlock* currentBlock = pFreeMemoryBlockList;
+		while (currentBlock)
+		{
+			if (currentBlock->BlockSize > largestSize)
+			{
+				largestSize = currentBlock->BlockSize;
+			}
+			currentBlock = currentBlock->pNextBlock;
+		}
+		return largestSize;
+	}
+
+	bool HeapManager::Contains(void* ptr) const
+	{
+		uintptr_t heapStart = reinterpret_cast<uintptr_t>(pEntireHeap->pBaseAddress);
+		uintptr_t heapEnd = heapStart + pEntireHeap->BlockSize;
+		uintptr_t address = reinterpret_cast<uintptr_t>(ptr);
+		return address >= heapStart && address < heapEnd;
+	}
+
+	bool HeapManager::IsAllocated(void* ptr) const
+	{
+		MemoryBlock* currentBlock = pOutstandingAllocationList;
+		while (currentBlock)
+		{
+			if (currentBlock->pBaseAddress == ptr)
+			{
+				return true;
+			}
+			currentBlock = currentBlock->pNextBlock;
+		}
+		return false;
+	}
+	
+	void HeapManager::Collect()
+	{
+		bool bShouldMerge;
+
+		do
+		{
+			bShouldMerge = false;
+			MemoryBlock* currentBlock = pFreeMemoryBlockList;
+
+			while (currentBlock && currentBlock->pNextBlock)
+			{
+				uintptr_t currentBlockEnd = reinterpret_cast<uintptr_t>(currentBlock->pBaseAddress) + currentBlock->BlockSize;
+				uintptr_t nextBlockStart = reinterpret_cast<uintptr_t>(currentBlock->pNextBlock->pBaseAddress);
+
+				// Check if the current block and the next block are adjacent
+				if (currentBlockEnd == nextBlockStart)
+				{
+					// Merge the blocks
+					currentBlock->BlockSize += currentBlock->pNextBlock->BlockSize;
+
+					// Remove the next block from the list
+					MemoryBlock* blockToRemove = currentBlock->pNextBlock;
+					currentBlock->pNextBlock = blockToRemove->pNextBlock;
+
+					// merging two blocks might create a new opportunity for further merging
+					bShouldMerge = true;
+				}
+				else
+				{
+					currentBlock = currentBlock->pNextBlock;
+				}
+			}
+		} while (bShouldMerge);
+	}
+
 	
 }
 
--- MonsterChase/Engine/HeapManager.h
+++ MonsterChase/Engine/HeapManager.h
@@ -2,6 +2,8 @@
 #define HEAP_ALLOCATOR_H
 
 #include <cstddef>
+#include <utility>
+#include <cassert>
 
 namespace HeapManagerProxy
 {
@@ -9,7 +11,7 @@ namespace HeapManagerProxy
     {
         void* pBaseAddress;
         size_t BlockSize;
-        struct MemoryBlock* pNextBlock;
+        MemoryBlock* pNextBlock;
     };
 
     class HeapManager
@@ -17,29 +19,81 @@ namespace HeapManagerProxy
     public:
         HeapManager(void* pHeapMemory, size_t heapSize, unsigned int numDescriptors);
         ~HeapManager();
+        
+        void ShowFreeBlocks() const;
+        void ShowOutstandingAllocations() const;
+        size_t GetLargestFreeBlock() const;
+        bool Contains(void* ptr) const;
+        bool IsAllocated(void* ptr) const;
+        void Collect();
 
-        void* alloc(size_t i_size);
-        bool IsAllocated(void* p_memory);
-        MemoryBlock* FindAllocatedMemoryBlock(void* p_memory);
-        void RemoveFromAllocatedList(MemoryBlock* memory_block);
-        void AddToFreeList(MemoryBlock* memory_block);
-        void MergeAdjacentFreeBlocks(MemoryBlock* memory_block);
-        bool free(void* pMemory);
-        bool Contains(void* pMemory) const;
-
+        void* alloc(size_t size);
+        bool free(void* ptr);
+    
     private:
-        MemoryBlock TheHeap;
+        
+        std::pair<MemoryBlock*, MemoryBlock*> findFreeBlock(size_t size);
+        
+        MemoryBlock* pEntireHeap;
         MemoryBlock* pFreeMemoryBlockList; // Linked list of free blocks
         MemoryBlock* pOutstandingAllocationList; // Linked list of allocated blocks
-
-        MemoryBlock* GetFreeMemoryBlock();
-        void TrackAllocation(MemoryBlock* pBlock);
-        // Other private helper methods
+        size_t theHeapSize;
     };
 
     HeapManager* CreateHeapManager(void* pHeapMemory, size_t heapSize, unsigned int numDescriptors);
+
+    void Destroy(HeapManager* pHeapManager);
+
+    inline void* alloc(HeapManager* pHeapManager, size_t size)
+    {
+        assert(pHeapManager != nullptr);
+
+        return pHeapManager->alloc(size);
+    }
+
+    inline void* alloc(HeapManager* pHeapManager, size_t size, size_t alignment)
+    {
+        assert(pHeapManager != nullptr);
+
+        return pHeapManager->alloc(size);
+    }
+
+    inline void Collect(HeapManager* pHeapManager)
+    {
+        pHeapManager->Collect();
+    }
+
+    inline bool free(HeapManager* pHeapManager, void* ptr)
+    {
+        return pHeapManager->free(ptr);
+    }
+
+    inline void ShowFreeBlocks(HeapManager* pHeapManager)
+    {
+        pHeapManager->ShowFreeBlocks();
+    }
+
+    inline void ShowOutstandingAllocations(HeapManager* pHeapManager)
+    {
+        pHeapManager->ShowOutstandingAllocations();
+    }
+
+    inline size_t GetLargestFreeBlock(HeapManager* pHeapManager)
+    {
+        return pHeapManager->GetLargestFreeBlock();
+    }
+
+    inline bool Contains(HeapManager* pHeapManager, void* ptr)
+    {
+        return pHeapManager->Contains(ptr);
+    }
+
+    inline bool IsAllocated(HeapManager* pHeapManager, void* ptr)
+    {
+        return pHeapManager->IsAllocated(ptr);
+    }
+
+    
 }
-
-
 
 #endif // HEAP_ALLOCATOR_H
